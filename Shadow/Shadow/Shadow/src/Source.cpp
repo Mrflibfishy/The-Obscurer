bb03549c450190290604c47cc066e12f7d542a67
Mrflibfishy <35642052+Mrflibfishy@users.noreply.github.com>
2018-02-05 21:14:13 +1300

    work

--- Shadow/Shadow/Shadow/src/Source.cpp
+++ Shadow/Shadow/Shadow/src/Source.cpp
@@ -70,6 +70,71 @@ int main(void)
 	}
 
 
+	float vertexData[] = {
+		-0.5f, -0.5f, 0.0f, 0.0f, 1.0f,
+		0.5f, -0.5f, 0.0f, 1.0f, 0.0f,
+		0.5f,  0.5f, 1.0f, 0.0f, 0.0f,
+		-0.5f,  0.5f, 0.0f, 1.0f, 0.0f
+	};
+
+
+	unsigned int indexData[] = {
+		0,1,2,
+		2,3,0
+	};
+
+	unsigned int vao;
+	GLCall(glGenVertexArrays(1, &vao));
+	GLCall(glBindVertexArray(vao));
+
+
+	VertexBuffer vb(vertexData, sizeof(float) * 5 * 4);
+	IndexBuffer ib(indexData, 6);
+
+
+	GLCall(glEnableVertexAttribArray(0));
+	GLCall(glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 5, 0));
+
+
+	GLCall(glEnableVertexAttribArray(1));
+	GLCall(glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 5, (GLvoid*)(sizeof(float) * 2)));
+
+
+	GLuint vertexShaderID = glCreateShader(GL_VERTEX_SHADER);
+	GLuint fragmentShaderID = glCreateShader(GL_FRAGMENT_SHADER);
+
+	const char* adapter[1];
+	std::string temp = readShaderCode("res/vertexShader.glsl");
+	adapter[0] = temp.c_str();
+	GLCall(glShaderSource(vertexShaderID, 1, adapter, 0));
+
+	temp = readShaderCode("res/fragmentShader.glsl");
+	adapter[0] = temp.c_str();
+	GLCall(glShaderSource(fragmentShaderID, 1, adapter, 0));
+
+	GLCall(glCompileShader(vertexShaderID));
+	GLCall(glCompileShader(fragmentShaderID));
+
+
+	if (!checkShaderStatus(vertexShaderID) || !checkShaderStatus(fragmentShaderID)) {
+		ASSERT(0);
+	}
+
+
+	GLuint programID = glCreateProgram();
+
+
+	glBindAttribLocation(programID, 0, std::string("vertexPos").c_str());
+	glBindAttribLocation(programID, 1, std::string("vertexColor").c_str());
+
+	GLCall(glActiveTexture(GL_TEXTURE0));
+	GLCall(glAttachShader(programID, vertexShaderID));
+	GLCall(glAttachShader(programID, fragmentShaderID));
+	GLCall(glLinkProgram(programID));
+
+	GLCall(glUseProgram(programID));
+
+
 
 
 	/* Loop until the user closes the window */
@@ -78,71 +143,7 @@ int main(void)
 		/* Render here */
 		GLCall(glClear(GL_COLOR_BUFFER_BIT));
 
-		float vertexData[] = {
-			-0.5f, -0.5f, 0.0f, 0.0f, 1.0f,
-			 0.5f, -0.5f, 0.0f, 1.0f, 0.0f,
-			 0.5f,  0.5f, 1.0f, 0.0f, 0.0f,
-			-0.5f,  0.5f, 0.0f, 1.0f, 0.0f
-		};
-
-
-		unsigned int indexData[] = {
-			0,1,2,
-			2,3,0
-		};
-
-		unsigned int vao;
-		GLCall(glGenVertexArrays(1, &vao));
-		GLCall(glBindVertexArray(vao));
-
-		VertexBuffer vb(vertexData, sizeof(float) * 5 * 4);
-
 		
-		IndexBuffer ib(indexData, 6);
-
-
-		GLCall(glEnableVertexAttribArray(0));
-		GLCall(glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 5, 0));
-
-
-		GLCall(glEnableVertexAttribArray(1));
-		GLCall(glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 5, (GLvoid*)(sizeof(float) * 2)));
-
-
-		GLuint vertexShaderID = glCreateShader(GL_VERTEX_SHADER);
-		GLuint fragmentShaderID = glCreateShader(GL_FRAGMENT_SHADER);
-
-		const char* adapter[1];
-		std::string temp = readShaderCode("res/vertexShader.glsl");
-		adapter[0] = temp.c_str();
-		GLCall(glShaderSource(vertexShaderID, 1, adapter, 0));
-
-		temp = readShaderCode("res/fragmentShader.glsl");
-		adapter[0] = temp.c_str();
-		GLCall(glShaderSource(fragmentShaderID, 1, adapter, 0));
-
-		GLCall(glCompileShader(vertexShaderID));
-		GLCall(glCompileShader(fragmentShaderID));
-
-
-		if (!checkShaderStatus(vertexShaderID) || !checkShaderStatus(fragmentShaderID)) {
-			ASSERT(0);
-		}
-
-
-		GLuint programID = glCreateProgram();
-
-
-		glBindAttribLocation(programID, 0, std::string("vertexPos").c_str());
-		glBindAttribLocation(programID, 1, std::string("vertexColor").c_str());
-
-		GLCall(glActiveTexture(GL_TEXTURE0));
-		GLCall(glAttachShader(programID, vertexShaderID));
-		GLCall(glAttachShader(programID, fragmentShaderID));
-		GLCall(glLinkProgram(programID));
-
-		GLCall(glUseProgram(programID));
-
 
 		GLCall(glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0));
 
